2401388877746a6a44b9b3f00229297f4b0f93f8
tim@mbp <timforrer16@gmail.com>
2025-04-06 16:13:50 +0900

    update links, .gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Text size='lg'>
         I <Link href="/projects">prod at the edges of quantum theory</Link> and see if what comes back can be used for building better quantum computers.<br />
         <br />
-        In my spare time, I <Link href="./">build LLMs</Link>, and try to take nice photos.
+        In my spare time, I <Link href="/">build LLMs</Link>, and try to take nice <Link href="/gallery">photos</Link>.
       </Text>
     </Stack>
   );
